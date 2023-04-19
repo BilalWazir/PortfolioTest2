54d4018c1577fb859221d14353dd90da3ad0c9f3
BilalWazir <101708971+BilalWazir@users.noreply.github.com>
2023-04-19 04:58:29 +0000

    updates to CSS and JS

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+/* Smooth Scroll Enabling Code*/
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
   
