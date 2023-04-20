503256201fed7e738415c1da962d712e7c84cd6c
BilalWazir <101708971+BilalWazir@users.noreply.github.com>
2023-04-20 06:29:32 +0000

    Chages to colour codes in CSS

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 /* Smooth Scroll Enabling Code*/
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-  
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
-  
+});
