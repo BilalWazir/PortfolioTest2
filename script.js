6f201c218ec6b2c96ad988ffbf1aa6e0da6ddf5e
BilalWazir <101708971+BilalWazir@users.noreply.github.com>
2023-04-21 16:27:17 +0000

    reverting all style with no animated cursor

--- script.js
+++ script.js
@@ -8,23 +8,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
-
-// Neon animated cursor
-function initNeonCursor() {
-  const neonCursor = threejsToys.neonCursor;
-  neonCursor({
-    el: document.getElementById('app'),
-    shaderPoints: 16,
-    curvePoints: 80,
-    curveLerp: 0.5,
-    radius1: 5,
-    radius2: 30,
-    velocityTreshold: 10,
-    sleepRadiusX: 100,
-    sleepRadiusY: 100,
-    sleepTimeCoefX: 0.0025,
-    sleepTimeCoefY: 0.0025
-  });
-}
-
-initNeonCursor();
