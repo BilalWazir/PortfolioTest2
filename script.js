b514943eac4b2fd05ef3a16ae5ee431302e33fa8
BilalWazir <101708971+BilalWazir@users.noreply.github.com>
2023-04-21 15:41:16 +0000

    Updated code for animated Cursor, new file cursor

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/* Smooth Scroll Enabling Code*/
-
+// Smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
@@ -10,5 +9,37 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
+// Custom cursor tracking
+const cursor = document.querySelector('.cursor');
+const menuItems = document.querySelectorAll('nav a');
 
-/*cursor animation*/
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = e.pageX + 'px';
+  cursor.style.top = e.pageY + 'px';
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('cursor-grow');
+  });
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('cursor-grow');
+  });
+});
+
+// Neon animated cursor
+import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
+
+neonCursor({
+  el: document.getElementById('app'),
+  shaderPoints: 16,
+  curvePoints: 80,
+  curveLerp: 0.5,
+  radius1: 5,
+  radius2: 30,
+  velocityTreshold: 10,
+  sleepRadiusX: 100,
+  sleepRadiusY: 100,
+  sleepTimeCoefX: 0.0025,
+  sleepTimeCoefY: 0.0025
+});
