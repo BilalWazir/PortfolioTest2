45c45d71bb6e217cb8e4147ec98efc8fa71f8a0e
BilalWazir <101708971+BilalWazir@users.noreply.github.com>
2023-04-22 05:50:40 +0000

    trying new JS for the typing code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
@@ -10,37 +9,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
+/* TEXT ANIMATION CODES */
 
-
-/* TEXT ANIMATION CODES*/
-
-
-/*typing text animation code*/
-
+/* Typing text animation code */
 const type_observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-      if (entry.isIntersecting) {
-          entry.target.classList.add('typing');
-      }
-  })
-})
-
-
-/*Sliding Text*/
-
-const slide_observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('animate');
+      const target = entry.target;
+      target.classList.add('typing');
+      type_observer.unobserve(target);
     }
   });
 });
 
-/*This is set in place so that animated text only plays when a user scrolls down to them*/
-
-slide_observer.observe(document.querySelector('.animated-text'));
-type_observer.observe(document.querySelector('.typing-text'));
-
+/* This is set in place so that animated text only plays when a user scrolls down to them */
 const typingTexts = document.querySelectorAll('.typing-text');
 typingTexts.forEach((text) => {
   type_observer.observe(text);
