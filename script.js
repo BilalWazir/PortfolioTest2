cb94fa3161eea067e264ad5dcd33696c909c35cb
BilalWazir <101708971+BilalWazir@users.noreply.github.com>
2023-04-22 05:22:36 +0000

    attempt 1 at typing text code

--- script.js
+++ script.js
@@ -10,4 +10,33 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
+
+
+/* TEXT ANIMATION CODES*/
+
+
 /*typing text animation code*/
+
+const type_observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+      if (entry.isIntersecting) {
+          entry.target.classList.add('typing');
+      }
+  })
+})
+
+
+/*Sliding Text*/
+
+const slide_observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate');
+    }
+  });
+});
+
+/*This is set in place so that animated text only plays when a user scrolls down to them*/
+
+slide_observer.observe(document.querySelector('.animated-text'));
+type_observer.observe(document.querySelector('.typing-text'));
